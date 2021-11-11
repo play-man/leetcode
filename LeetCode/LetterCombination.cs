d8d780c9cff5e0eb3c787ae38222657b06661555
Oleksandr Sereda <oleksandr.sereda@abbyy.com>
2021-11-11 19:35:04 +0200

    improvements

--- LeetCode/LetterCombination.cs
+++ LeetCode/LetterCombination.cs
@@ -14,10 +14,10 @@ namespace LeetCode
             { '3', new List<char>() {'d', 'e', 'f'}},
             { '4', new List<char>() {'g', 'h', 'i'}},
             { '5', new List<char>() {'j', 'k', 'l'}},
-            { '6', new List<char>() {'a', 'b', 'c'}},
-            { '7', new List<char>() {'a', 'b', 'c'}},
-            { '8', new List<char>() {'a', 'b', 'c'}},
-            { '9', new List<char>() {'a', 'b', 'c'}},
+            { '6', new List<char>() {'m', 'n', 'o'}},
+            { '7', new List<char>() {'p', 'q', 'r', 's'}},
+            { '8', new List<char>() {'t', 'u', 'v'}},
+            { '9', new List<char>() {'w', 'x', 'y', 'z'}},
         };
         public static IList<string> LetterCombinations(string digits)
         { 
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -10,22 +10,6 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            int[] nums1 = new int[] { -1, 0, 1, 2, -1, -4 };
-            int[] nums2 = new int[] { -1, 0, 1, 2, -1, -4 };
-            //Console.WriteLine((new MedianTwoArrays()).FindMedianSortedArrays(nums1, nums2));
-            //Console.WriteLine((new Atoi().MyAtoi("-5.-")));
-            //Console.WriteLine(LeapYears.MonthCount(new DateTime(1901, 1, 1), new DateTime(1999, 12, 1), 1, 2));
-            //Console.WriteLine(StockSeller.MaxProfit(new int[] { 3, 1, 2, 6, 3, 4, 6 }));
-            //Console.WriteLine(StockSellerIII.MaxProfit(new int[] { 14, 9, 10, 12, 4, 8, 1, 16 }));
-            //Console.WriteLine(LongestPalindromeClass.LongestPalindrome("abbbbbbbbbba"));
-            /*Console.WriteLine(MinHeight.FindMinHeightTrees(6, new int[][] {
-                new int[] { 0, 1 },
-                new int[] { 0, 2 },
-                new int[] { 0, 3 },
-                new int[] { 3, 4 },
-                new int[] { 4, 5 },
-            })[0]);*/
-            Console.WriteLine(Divisors.GCD(-5, -3));
             Console.ReadKey();
         }
     }
