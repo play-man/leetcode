42503e805305c7843bda4017f43b3d73894f6696
play-man <fun.fic.tio.nal@gmail.com>
2021-11-18 21:55:37 +0200

    problems 18.11

---
+++ LeetCode/Anagrams.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class Anagrams
+    {
+        /*
+         Given two strings s and p, return an array of all the start indices of p's anagrams in s. 
+        You may return the answer in any order.
+
+        An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+        typically using all the original letters exactly once.
+        
+        Constraints:
+
+        1 <= s.length, p.length <= 3 * 10^4
+        s and p consist of lowercase English letters.*/
+        public IList<int> FindAnagrams(string s, string p)
+        {
+            IList<int> result = new List<int>();
+            int pLen = p.Length;
+            int sLen = s.Length;
+            if (sLen >= pLen)
+            {
+                // Stores letters of p that are missing in the substring of s [i, i+p]
+                List<char> currentWordDifference = new List<char>();
+                // Stores letter of s that couldn't be deleted from currentWordDifference
+                // because they are yet to be added there
+                // Example: s = "abac", p = "abc"
+                // Since "aba" is missing c, we'll need to delete c later as we iterate 
+                // through pLen-substrings of s
+                List<char> charsToDelete = new List<char>();
+                for (int i = 0; i < pLen; ++i)
+                    currentWordDifference.Add(s[i]);
+                for (int i = 0; i < pLen; ++i)
+                {
+                    if (!currentWordDifference.Remove(p[i]))
+                        charsToDelete.Add(p[i]);
+                }
+                if (currentWordDifference.Count == 0)
+                    result.Add(0);
+                for (int i = pLen; i < sLen; ++i)
+                {
+                    currentWordDifference.Add(s[i]);
+                    for (int j = 0; j < charsToDelete.Count; ++j)
+                    {
+                        if (currentWordDifference.Remove(charsToDelete[j]))
+                        {
+                            charsToDelete.RemoveAt(j);
+                            j--;
+                        }
+                    }
+                    if (!currentWordDifference.Remove(s[i - pLen]))
+                        charsToDelete.Add(s[i - pLen]);
+                    if (currentWordDifference.Count == 0)
+                        result.Add(i - pLen + 1);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCode/NumbersDisappeared.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    /*Given an array nums of n integers where nums[i] is in the range [1, n], 
+     * return an array of all the integers in the range [1, n] that do not appear in nums.
+     
+     Constraints:
+
+    n == nums.length
+    1 <= n <= 10^5
+    1 <= nums[i] <= n*/
+    internal static class NumbersDisappeared
+    {
+        public static IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            HashSet<int> numsHashSet = Enumerable.Range(1, nums.Length).ToHashSet();
+
+            for (int i = 0; i < nums.Length; i++)
+                numsHashSet.Remove(nums[i]);
+
+            return numsHashSet.ToList();
+        }
+    }
+}
